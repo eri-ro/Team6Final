4b27293914b98816b41adc5eac66a661037b7118
eri-ro <er272456@ucf.edu>
2026-04-01 16:51:49 -0400

    Gravity Shift Mechanic

---
+++ Team6Final/Assets/Scripts/GravityShift/GravitySurface.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+// Put this on objects that can be targeted for gravity shifts.
+public class GravitySurface : MonoBehaviour
+{
+}
---
+++ Team6Final/Assets/Scripts/GravityShift/GravityWorld.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+// Current "up" direction; Physics.gravity pulls along -Up.
+public class GravityWorld
+{
+    public static Vector3 Up { get; private set; } = Vector3.up;
+
+    public static void SetGravityUp(Vector3 worldUp)
+    {
+        Up = worldUp.normalized;
+        Physics.gravity = -Up * 9.81f; // can make 9.8 a variable to adjust gravity
+    }
+}
